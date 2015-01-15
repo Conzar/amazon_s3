72ec93c45c8331d6090c23a8cfcc5bc2928b55c6
Michael Speth <mspeth@monky-games.com>
2015-01-16 10:50:56 +1300

    fixed style

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 2015-01-16 Release 0.1.0
+### Summary
+Initial release.
--- tests/init.pp
+++ tests/init.pp
@@ -9,4 +9,11 @@
 # Learn more about module testing here:
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
-include amazon_s3
+class {'amazon_s3':
+  aws_access_key    => 'ACCESS_KEY',
+  secret_access_key => 'SECRET_KEY',
+}
+amazon_s3::s3_mount{'s3_mount':
+  mount_point => '/mnt/s3_mount',
+  require     => Class['amazon_s3'],
+}
