9c9b62a254880141fc0428f8921e384b824a46e3
Ilya Kulakov <kulakov.ilya@gmail.com>
2015-01-28 22:24:09 +0600

    Allow a user to unmount and specify options of the mount.

--- manifests/s3_mount.pp
+++ manifests/s3_mount.pp
@@ -5,29 +5,36 @@
 #
 # === Parameters
 #
-# [*s3_bucket_name*]
-#   The name of the s3 bucket. By default, it uses the title.
+# [*ensure*]
+#   Control what to do with this mount. Acceps the same values as the 'mount' type.
 #
 # [*mount_point*]
 #   The path to mount.  Note, this module ensures the directory exists.
 #
+# [*options*]
+#   Options used when mounting.
+#
+# [*s3_bucket_name*]
+#   The name of the s3 bucket. By default, it uses the title.
+#
 define amazon_s3::s3_mount(
+  $ensure = 'mounted',
   $mount_point,
+  $options = 'allow_other',
   $s3_bucket_name = $title,
 ){
-  
   file{$mount_point:
     ensure => directory,
     mode   => '0777',
   }
-  
+
   # mount the s3 bucket
   mount { $title:
-    ensure  => 'mounted',
+    ensure  => $ensure,
     name    => $mount_point,
     device  => "s3fs#${s3_bucket_name}",
     fstype  => 'fuse',
-    options => 'allow_other',
+    options => $options,
     require => File[$mount_point],
   }
 }
